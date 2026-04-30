ecf3a91f7a54846d608743c3965a2af5710c54a7
Archi1508 <143097029+Archi1508@users.noreply.github.com>
2026-04-30 20:50:42 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int, int>mpp;
+        for(auto x:nums)
+        {
+            mpp[x]++;
+        }
+        for(auto it:mpp)
+        {
+            if(it.second>n/2)
+            {
+                return it.first;
+            }
+        }
+    }
+};
